7f2e08cd0fade1e6319aa58185f6a5a4739a78c3
Olaniyi Ajayi <ajayiolaniyi8@gmail.com>
2024-03-06 16:17:41 +0200

    properties pageupdated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,22 @@
-import Navbar from '@/components/Navbar';
-import '@/assets/styles/globals.css';
-import Footer from '@/components/footer';
-
+import Navbar from "@/components/Navbar";
+import "@/assets/styles/globals.css";
+import Footer from "@/components/footer";
 
 export const metadata = {
-  title: 'PropertyPulse | Find The Perfect Rebtal',
-  Description: 'Find your dream rental property',
+  title: "PropertyPulse | Find The Perfect Rebtal",
+  Description: "Find your dream rental property",
 };
 
-const Mainlayout = ({children}) => {
+const Mainlayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-          <Navbar />
+    <html lang="en">
+      <body>
+        <Navbar />
         <main>{children}</main>
-        <Footer/>
-        </body>
+        <Footer />
+      </body>
     </html>
-    
-  )
-}
+  );
+};
 
 export default Mainlayout;
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Propertie Page</h1>
-      <Link href="/">Go to home page</Link>
-    </div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No proprties found</p>
+        ): (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.map((property) => (
+            <PropertyCard key={property._id} property={property}/>
+          ))}
+        </div>
+        )}
+        
+      </div>
+    </section>
   );
 };
 
